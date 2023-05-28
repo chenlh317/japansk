4ab2a30d3c42869c142a7c8fdd5a557a82f6bd4e
Linghua Chen <chenlh317@gmail.com>
2023-05-28 15:03:24 +0200

    add

--- remove_empty_br.py
+++ remove_empty_br.py
@@ -2,7 +2,7 @@
 """
 Remove redundant <br> in .md files.
 
-Run with `python remove_empty_br.py`.
+Run in terminal with `python remove_empty_br.py`.
 
 """
 
@@ -11,14 +11,17 @@ import os
 from pathlib import Path
 
 
-def remove_empty_line_break(file_name):
+def remove_empty_line_break(file_name: Path):
     """
     Search for and remove redundant line breaks `<br>` and `<br>\n`.
+
+    Args:
+        file_name(Path): file to process
+    Returns:
+        None
     """
 
     lines = []
-
-    msg = f"Found empty line break in {file_name}"
 
     # read in all lines of the file
     with open(file_name, "r") as f:
@@ -27,8 +30,11 @@ def remove_empty_line_break(file_name):
     with open(file_name, "w") as f:
 
         # check redundant line breaks for each line
-        for line in lines:
+        for line_nr, line in enumerate(lines):
 
+            msg = f"Removed empty line break in {file_name}: Line {line_nr}"
+
+            # removed <br> if that's the only thing in the line
             if line == "<br>\n":
                 print(msg)
                 f.write("\n")
@@ -40,10 +46,16 @@ def remove_empty_line_break(file_name):
     return None
 
 
-def process_all_files(root_dir, ext):
+def process_all_files(root_dir: Path, ext: tuple):
     """
     Loop all files and process.
     Filter by extensions.
+
+    Args:
+        root_dir(Path): directory to search
+        ext(tuple): tuple of file extensions to include in the search
+    Returns:
+        None
     """
 
     for subdir, _, files in os.walk(root_dir):
