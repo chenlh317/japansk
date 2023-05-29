a685e6ea9ffe21d976174a13ea56ec88d226c0ce
Linghua Chen <chenlh317@gmail.com>
2023-05-29 22:04:33 +0200

    updates

--- build_lyrics.bat
+++ build_lyrics.bat
@@ -1,5 +1,10 @@
 
-REM Build lyrics and process with ghp-import
+REM Format and build lyrics, and process with ghp-import
+
+REM Run in VSCode terminal with cmd.exe -/c "build_lyrics.bat"
+
+
+python process_text.py
 
 jb build lyrics
 
