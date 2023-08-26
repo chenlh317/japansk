5ac15b0e366d2ea8235fe16f64265bc558d6d81f
Linghua Chen <chenlh317@gmail.com>
2023-08-26 16:27:54 +0200

    sync pages

---
+++ new_lyrics.bat
@@ -0,0 +1,26 @@
+
+REM Format and build lyrics, and process with ghp-import
+
+REM Run in VSCode terminal with below
+REM cmd.exe -/c "new_lyrics.bat"
+
+
+python process_text.py
+
+jb build lyrics
+
+ghp-import -n -p -f lyrics/_build/html
+
+git checkout main
+git add .
+git commit -m "updates"
+git push origin main
+
+git checkout gh-pages
+git add .
+git commit -m "sync pages"
+git push origin gh-pages
+
+git checkout main
+
+cmd /k
