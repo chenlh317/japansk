46d46e59b7a2bd82b6bd66545b057371627c64b9
Linghua Chen <chenlh317@gmail.com>
2025-11-01 23:26:33 +0100

    debug slice mp3

--- slice_mp3.py
+++ slice_mp3.py
@@ -1,6 +1,11 @@
 """
 Slices an audio file and exports the sliced segment.
 
+Need to make sure ffmpeg is installed:
+    https://ffmpeg.org/download.html
+And set system PATH accordingly in environment variables.
+E.g. on Windows, add something like `C:/ffmpeg-8.0-full_build/bin` to PATH.
+
 Run by `python slice_mp3.py`
 
 """
@@ -44,5 +49,5 @@ if __name__ == "__main__":
     slice_mp3_audio(
         song_path=Path("kokia.mp3"),
         start_second=0,
-        end_second=338,
+        end_second=218,
     )
